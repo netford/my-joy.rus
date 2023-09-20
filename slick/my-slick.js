1b5893daaea8c4d996e1f9060a8a85509e08179e
konstpon <konstpon2012@gmail.com>
2023-09-20 16:27:24 +0300

    Первичная инициализация

---
+++ slick/my-slick.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.slider-for-action').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2500
+    });
+});
